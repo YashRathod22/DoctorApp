0dcd709b838b50e2107a1f156f3a02fe759eedfc
YashRathod22 <yashrathod0226@gmail.com>
2024-11-22 17:54:33 +0530

    added lottie react native animation on formsubmission screen

--- src/screens/FormSubmission.tsx
+++ src/screens/FormSubmission.tsx
@@ -2,12 +2,18 @@ import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import {lightGreen, white} from '../utils/Color';
 import Octicons from 'react-native-vector-icons/Octicons';
-
+import LottieView from 'lottie-react-native';
 const FormSubmission = () => {
   return (
     <View style={styles.container}>
       <View style={styles.icon}>
-        <Octicons name="check-circle" size={70} color={lightGreen} />
+        {/* <Octicons name="check-circle" size={70} color={lightGreen} /> */}
+        <LottieView
+          style={{flex: 1}}
+          source={require('../../assets/check.json')}
+          autoPlay
+          loop
+        />
       </View>
       <Text style={styles.text}>Thank You!</Text>
       <Text style={styles.text2}>Your Submission has been recieved.</Text>
@@ -35,6 +41,8 @@ const styles = StyleSheet.create({
   icon: {
     alignSelf: 'center',
     marginTop: '20%',
+    width: 110,
+    height: 110,
   },
   text: {
     textAlign: 'center',
